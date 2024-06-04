df7eacbcf4b854980c8a934e1d17f8145309f4c1
ali balochi <alibalochi191077@gmail.com>
2024-06-04 15:28:59 +0330

    create getAllCustomerData functin

---
+++ dataFetching/fetchCustomersData.ts
@@ -0,0 +1,32 @@
+import {useEffect} from 'react'
+import { useQuery } from 'react-query';
+import useStore,{Customer} from '@/store/store'
+import { Unkempt } from 'next/font/google';
+
+export const getAllCustomers = () => {
+   const fetchCustomers = async (): Promise<Customer[]> => {
+    const response = await fetch("http://localhost:9000/customers");
+   if (!response.ok) {
+     throw new Error("Network response was not ok");
+   }
+   const data = await response.json();
+   return data;
+  };
+
+  const setCustomers = useStore((set) => set.setCustomers)
+  const customers:Customer[] = useStore((set) => set.customers)
+
+  const {data,error,isLoading} = useQuery<Customer[]>('customers',fetchCustomers)
+
+  useEffect(() => {
+    if(data) {
+        setCustomers(data)
+    }
+  },[data,setCustomers])
+  
+  if (!isLoading) {
+    return customers
+  }  
+
+  return null
+}
