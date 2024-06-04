072164dd030f02bfbffec54b52b3121857460f62
ali balochi <alibalochi191077@gmail.com>
2024-06-04 22:43:01 +0330

    create navbar

--- Components/Body.tsx
+++ Components/Body.tsx
@@ -1,15 +1,19 @@
 import CustomerComponent from "./CustomerComponents/CustomerComponent";
+import NavBar from "./Navbar/Navbar";
 import SearchComponent from "./Search/SearchComponent";
 const Body: React.FC = () => {
   return (
-    <div className=" w-full h-screen pt-12">
-      <div className=" w-full flex justify-center ">
-        <SearchComponent />
+    <>
+      <NavBar />
+      <div className=" w-full h-screen pt-12">
+        <div className=" w-full flex justify-center ">
+          <SearchComponent />
+        </div>
+        <div className=" w-full">
+          <CustomerComponent />
+        </div>
       </div>
-      <div className=" w-full">
-        <CustomerComponent />
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ Components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Add } from "@mui/icons-material";
+const NavBar: React.FC = () => {
+  return (
+    <div className=" w-full bg-slate-800 h-16 flex justify-between items-center py-5">
+      <h1>Account Boock</h1>
+
+      <button className=" bg-blue-600 text-white px-2 py-1 rounded-md items-center mx-2 shadow-md shadow-blue-500 active:shadow-none">
+        <Add />
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
