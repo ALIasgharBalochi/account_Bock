d9605a4f05910a3e4c7b7ef8c04627cd7e251770
ali balochi <alibalochi191077@gmail.com>
2024-06-05 18:22:52 +0330

    create navbar

--- Components/Navbar/Navbar.tsx
+++ Components/Navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 import { Add } from "@mui/icons-material";
+import Link from "next/link";
 const NavBar: React.FC = () => {
   return (
     <div className=" w-full bg-slate-800 h-16 flex justify-between items-center py-5">
       <h1>Account Boock</h1>
-
-      <button className=" bg-blue-600 text-white px-2 py-1 rounded-md items-center mx-2 shadow-md shadow-blue-500 active:shadow-none">
-        <Add />
-      </button>
+      <Link href="/createCustomer">
+        <button className=" bg-blue-600 text-white px-2 py-1 rounded-md items-center mx-2 shadow-md shadow-blue-500 active:shadow-none">
+          <Add />
+        </button>
+      </Link>
     </div>
   );
 };
