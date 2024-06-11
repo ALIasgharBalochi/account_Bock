b8bbc95903e09ad96d09568c5d444674fc8bab25
ali balochi <alibalochi191077@gmail.com>
2024-06-11 20:47:53 +0330

    changed in createCustomer function

---
+++ dataFetching/createCustomer.ts
@@ -0,0 +1,16 @@
+export const createCustomerFunc = async (data:FormData):Promise<any> => {
+    const res = await fetch('http://localhost:9000/customers', {
+        method: 'POST',
+        // headers: {
+        //     'Content-Type': 'application/json',
+        // },
+        body:data 
+    })
+
+    if (!res.ok) {
+        throw new Error('Network response was not ok')
+    }
+
+    const d = res.json()
+    return d
+}
