22996353c15c2a63f4cc59d3863af7f727a71fe4
ali balochi <alibalochi191077@gmail.com>
2024-06-13 18:49:39 +0330

    add Loading and not found in Customer component

--- Components/CustomerComponents/CustomerComponent.tsx
+++ Components/CustomerComponents/CustomerComponent.tsx
@@ -1,5 +1,7 @@
 import { Customer } from "@/store/store";
 import CustomerCardContainer from "./CustomerCard/CustomerCardContiner";
+import Loading from "@/app/loading";
+import NotFound from "@/app/not-found";
 type Props = {
   Customers: Customer[];
   loading: boolean;
@@ -17,11 +19,15 @@ const CustomerComponent: React.FC<Props> = ({
       {!loading ? (
         <>
           {query.text.length > 0 ? (
-            <div className=" w-full items-center grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-1 p-5">
-              {searchedCustomers?.map((customer: Customer) => (
-                <CustomerCardContainer data={customer} />
-              ))}
-            </div>
+            searchedCustomers.length > 0 ? (
+              <div className=" w-full items-center grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-1 p-5">
+                {searchedCustomers?.map((customer: Customer) => (
+                  <CustomerCardContainer data={customer} />
+                ))}
+              </div>
+            ) : (
+              <NotFound text="مشتری پیدا نشد " />
+            )
           ) : (
             <div className=" w-full items-center grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-1 p-5">
               {Customers?.map((customer: Customer) => (
@@ -31,7 +37,7 @@ const CustomerComponent: React.FC<Props> = ({
           )}
         </>
       ) : (
-        <h1>loading ...</h1>
+        <Loading />
       )}
     </div>
   );
