ea00c4d6b12a819b39c0de3576119db2ac1ed3e6
ali balochi <alibalochi191077@gmail.com>
2024-06-13 18:25:17 +0330

    create loading component

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-blue-700 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
+}
