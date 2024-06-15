2c107fc5b8512e656a1530464095dc2a776a69f0
ali balochi <alibalochi191077@gmail.com>
2024-06-15 19:40:25 +0330

    add Link

--- Components/CustomerComponents/CustomerCard/CustomerCardAction.tsx
+++ Components/CustomerComponents/CustomerCard/CustomerCardAction.tsx
@@ -1,10 +1,16 @@
-const CustomerCardAction: React.FC = () => {
+import Link from "next/link";
+type Props = {
+  customerId: number;
+};
+const CustomerCardAction: React.FC<Props> = ({ customerId }) => {
   return (
     <>
       <div className="flex mt-4 md:mt-6">
-        <button className="py-2 px-8 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-          مشاهده حساب مشتری
-        </button>
+        <Link href={`http://localhost:3000/customer/${customerId}`}>
+          <button className="py-2 px-8 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            مشاهده حساب مشتری
+          </button>
+        </Link>
       </div>
     </>
   );
