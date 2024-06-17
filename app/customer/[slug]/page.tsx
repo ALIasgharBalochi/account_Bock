6e898901b8792103e8de63d0c78f872bae06ae13
ali balochi <alibalochi191077@gmail.com>
2024-06-17 22:40:19 +0330

    get customer and get debts and send for customer page

--- app/customer/[slug]/page.tsx
+++ app/customer/[slug]/page.tsx
@@ -1,19 +1,39 @@
 "use client";
-
-import useStore, { Customer } from "@/store/store";
-
+import { useEffect, useState } from "react";
+import { useFetchCustomerById } from "@/dataFetching/fetchCustomersData";
+import Loading from "@/app/loading";
+import { Customer, Debt } from "@/store/store";
+import CustomerContainerPage from "../CustomerComponent/CustomerContainerPage";
+import { useFetchDebts } from "@/dataFetching/fetchDebtsData";
 export default function customer({ params }: { params: { slug: number } }) {
   const customerId = params.slug;
 
-  const getCustomerById = useStore((state) => state.getCustomerById);
-  const customer: Customer | undefined = getCustomerById(customerId);
+  const [customer, setCustomer] = useState<Customer | null>(null);
+  const [customerDebts, setCustomerDebts] = useState<Debt[] | []>([]);
+
+  const { data, isLoading, error } = useFetchCustomerById(customerId);
+  const { data: debts, isLoading: loadingDebts } = useFetchDebts();
+  useEffect(() => {
+    if (data) {
+      setCustomer(data);
+    }
+  }, [isLoading, data]);
+
+  useEffect(() => {
+    if (debts) {
+      const d = debts.filter((debt: Debt) => {
+        return debt.customer._id === customerId;
+      });
+      setCustomerDebts(d);
+    }
+  }, [loadingDebts, debts]);
 
   return (
     <>
-      {customer != undefined ? (
-        <h1>hello {customer.name}</h1>
+      {customer != null ? (
+        <CustomerContainerPage customer={customer} debts={customerDebts} />
       ) : (
-        <h1>customer not found</h1>
+        <Loading />
       )}
     </>
   );
