42818a7fbe76b9a8c4ce2b299b246c9deeb58777
ali balochi <alibalochi191077@gmail.com>
2024-06-18 23:06:12 +0330

    create button for open modal

---
+++ app/customer/AddDebtButton.tsx
@@ -0,0 +1,21 @@
+import { Add } from "@mui/icons-material";
+import { Dispatch, SetStateAction } from "react";
+type Props = {
+  setOpneModal: Dispatch<SetStateAction<boolean>>;
+};
+const AddDebtButton: React.FC<Props> = ({ setOpneModal }) => {
+  return (
+    <div className=" p-3 bg-blue-600 rounded-md bottom-3 right-3 w-10 h-10 flex justify-center items-center absolute">
+      <button
+        data-ripple-light="true"
+        data-dialog-target="dialog"
+        className="active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        onClick={() => setOpneModal(true)}
+      >
+        <Add />
+      </button>
+    </div>
+  );
+};
+
+export default AddDebtButton;
