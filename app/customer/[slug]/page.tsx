0bd25a3642a67875bf30277fae0e63af49580553
ali balochi <alibalochi191077@gmail.com>
2024-06-18 23:09:01 +0330

    add modal and button to page and sent data to create Debt func

--- app/customer/[slug]/page.tsx
+++ app/customer/[slug]/page.tsx
@@ -4,15 +4,53 @@ import { useFetchCustomerById } from "@/dataFetching/fetchCustomersData";
 import Loading from "@/app/loading";
 import { Customer, Debt } from "@/store/store";
 import CustomerContainerPage from "../CustomerComponent/CustomerContainerPage";
-import { useFetchDebts } from "@/dataFetching/fetchDebtsData";
+import { useFetchDebts, createDebt } from "@/dataFetching/fetchDebtsData";
+import AddDebtButton from "../AddDebtButton";
+import Modal from "../Modal";
+import { useMutation } from "react-query";
+import { toast } from "react-toastify";
+import Router from "next/router";
 export default function customer({ params }: { params: { slug: number } }) {
   const customerId = params.slug;
 
   const [customer, setCustomer] = useState<Customer | null>(null);
   const [customerDebts, setCustomerDebts] = useState<Debt[] | []>([]);
 
+  //  open Modal
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const { data, isLoading, error } = useFetchCustomerById(customerId);
   const { data: debts, isLoading: loadingDebts } = useFetchDebts();
+
+  const mutation = useMutation(createDebt, {
+    onSuccess: () => {
+      toast.success(" کالا با موفقیت ساخته شد ", {
+        position: "top-right",
+      });
+    },
+    onError: (error: unknown) => {
+      if (error instanceof Error) {
+        console.log("Error creating customer: ", error.message);
+      }
+    },
+  });
+
+  const onClose = (): void => {
+    setIsOpen(false);
+  };
+
+  const onSubmit = (data: {
+    customer: number;
+    itemName: string;
+    number: number;
+    unit: string;
+    part: number;
+    discount: string;
+    itemPrice: number;
+    byWhom: string;
+  }) => {
+    mutation.mutate(data);
+  };
   useEffect(() => {
     if (data) {
       setCustomer(data);
@@ -35,6 +73,13 @@ export default function customer({ params }: { params: { slug: number } }) {
       ) : (
         <Loading />
       )}
+      <AddDebtButton setOpneModal={setIsOpen} />
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={onSubmit}
+        customerId={customerId}
+      />
     </>
   );
 }
