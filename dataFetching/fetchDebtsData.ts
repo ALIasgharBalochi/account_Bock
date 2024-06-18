986f4dad94079cda5ccb4dcfb75f5def8ebcb51f
ali balochi <alibalochi191077@gmail.com>
2024-06-18 23:06:47 +0330

    create function for create Debt

--- dataFetching/fetchDebtsData.ts
+++ dataFetching/fetchDebtsData.ts
@@ -15,3 +15,29 @@ const fetchDebts = async ():Promise<Debt[]> => {
 export const useFetchDebts = () => {
     return useQuery('debts',fetchDebts)
 }
+
+export const createDebt = async (debt: {
+    customer: number;
+    itemName: string;
+    number: number;
+    unit: string;
+    part: number;
+    discount: string;
+    itemPrice: number;
+    byWhom: string;
+  }) => {
+    const res = await fetch('http://localhost:9000/debts',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(debt)
+    })
+    
+    if (!res.ok) {
+        throw new Error('failed to create debt')
+    }
+
+    const d = res.json()
+    return d
+}
