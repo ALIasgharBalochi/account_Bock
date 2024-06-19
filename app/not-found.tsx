bc9820a676a5e8294c42eb7728f8429c47008d28
ali balochi <alibalochi191077@gmail.com>
2024-06-19 21:17:42 +0330

    get height of props

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,15 @@ import notFoundImg from "@/public/notfound.png";
 import Image from "next/image";
 export default function NotFound({
   text = "page not found",
+  height = "screen",
 }: {
   text: string;
+  height: string;
 }) {
   return (
-    <div className=" w-full h-screen flex flex-col justify-center items-center">
+    <div
+      className={` w-full h-${height} flex flex-col justify-center items-center`}
+    >
       <Image src={notFoundImg} width={400} height={300} alt="not-found" />
       <h1>{text}</h1>
     </div>
