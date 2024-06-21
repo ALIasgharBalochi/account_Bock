e652cea3fbd54421aa916be1573a9917c30883b7
ali balochi <alibalochi191077@gmail.com>
2024-06-21 15:34:08 +0330

    change in debt type

--- dataFetching/fetchDebtsData.ts
+++ dataFetching/fetchDebtsData.ts
@@ -2,30 +2,23 @@ import { Debt } from "@/store/store"
 import { useQuery } from "react-query"
 
 
-const fetchDebts = async ():Promise<Debt[]> => {
+export const fetchDebts = async ():Promise<Debt[]> => {
     const res = await fetch('http://localhost:9000/debts')
 
     if (!res.ok){
         throw new Error('fetchDebts:failed to fetch')
     }
 
-    return res.json()
+    const d = res.json()
+
+    return d
 }
 
 export const useFetchDebts = () => {
     return useQuery('debts',fetchDebts)
 }
 
-export const createDebt = async (debt: {
-    customer: number;
-    itemName: string;
-    number: number;
-    unit: string;
-    part: number;
-    discount: string;
-    itemPrice: number;
-    byWhom: string;
-  }) => {
+export const createDebt = async (debt:Debt) => {
     const res = await fetch('http://localhost:9000/debts',{
         method: 'POST',
         headers: {
