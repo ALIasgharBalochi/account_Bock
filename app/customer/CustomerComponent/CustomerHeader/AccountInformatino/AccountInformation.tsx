35aebb1de3766298773ba00045001cd31cde1e72
ali balochi <alibalochi191077@gmail.com>
2024-06-29 19:47:08 +0330

    Getting the last payment and displaying it

--- app/customer/CustomerComponent/CustomerHeader/AccountInformatino/AccountInformation.tsx
+++ app/customer/CustomerComponent/CustomerHeader/AccountInformatino/AccountInformation.tsx
@@ -1,13 +1,29 @@
 import { Customer } from "@/store/store";
-
+import { useLatestPayment } from "@/dataFetching/fetchPaymentData";
+import ShowTime from "@/Components/ShowTime";
 type Props = {
   customer: Customer;
 };
+export type Pyment = {
+  _id: string;
+  customer: Customer;
+  amount: number;
+  date: string;
+};
 const AccountInformation: React.FC<Props> = ({ customer }) => {
+  const { data, isLoading, error } = useLatestPayment(customer._id);
+
   return (
     <div className=" flex justify-start">
       <div className=" text-end">
-        <p>0$ 00,00,00:مبلغ و تاریخ اخرین پرداختی </p>
+        {isLoading && data == undefined ? (
+          <p>تاریخ و مبلغ اخرین پرداختی: 0$ 00:00:00</p>
+        ) : (
+          <p>
+            {data?.amount}$ <ShowTime timestamp={data?.date} />
+            :date and lest payment{" "}
+          </p>
+        )}
         <p> {customer.totalDebt}:مجموع کل حساب</p>
       </div>
     </div>
