3c99160d6a58e4dcac54e7e5b2706d552dc2445d
ali balochi <alibalochi191077@gmail.com>
2024-07-02 18:23:05 +0330

    create layout

---
+++ Components/Layout.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import PaginationComponent from "./PaginationComponent";
+const Layout: React.FC = () => {
+  return <PaginationComponent CustomerPerPage={6} />;
+};
+
+export default Layout;
