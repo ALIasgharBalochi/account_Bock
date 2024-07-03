ccbfcf4d4d5ab5aae5d6c31320834bc6d73f75db
ali balochi <alibalochi191077@gmail.com>
2024-07-03 18:56:28 +0330

    refactoring: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "./ReactQueryPrivider";
 import ToastProvider from "./ToastifyProvider";
+import NavBar from "@/Components/Navbar/Navbar";
 const vazir = Vazirmatn({
   subsets: ["arabic", "latin"],
   display: "swap",
@@ -22,7 +23,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={vazir.className}>
         <ToastProvider>
-          <ReactQueryProvider>{children}</ReactQueryProvider>
+          <ReactQueryProvider>
+            <NavBar />
+            {children}
+          </ReactQueryProvider>
         </ToastProvider>
       </body>
     </html>
