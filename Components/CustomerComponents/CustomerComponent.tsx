094458f97f5cc49fb3d28fa5e5cb0b6980c8c70a
ali balochi <alibalochi191077@gmail.com>
2024-07-03 19:13:20 +0330

    refactoring : customer component

--- Components/CustomerComponents/CustomerComponent.tsx
+++ Components/CustomerComponents/CustomerComponent.tsx
@@ -1,51 +1,21 @@
 import { Customer } from "@/store/store";
 import CustomerCardContainer from "./CustomerCard/CustomerCardContiner";
 import Loading from "@/app/loading";
-import NotFound from "@/app/not-found";
 type Props = {
   Customers: Customer[];
   loading: boolean;
-  searchedCustomers: Customer[];
-  query: { text: string };
 };
-const CustomerComponent: React.FC<Props> = ({
-  Customers,
-  searchedCustomers,
-  query,
-  loading,
-}) => {
+const CustomerComponent: React.FC<Props> = ({ Customers, loading }) => {
   return (
     <div>
       {!loading ? (
-        <>
-          {query.text.length > 0 ? (
-            searchedCustomers.length > 0 ? (
-              <div className=" w-full items-center grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-1 p-5">
-                {searchedCustomers?.map((customer: Customer, index) => (
-                  <div key={index}>
-                    <CustomerCardContainer data={customer} />
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <NotFound text="مشتری پیدا نشد " height="screen" />
-            )
-          ) : (
-            <>
-              {Customers?.length > 0 ? (
-                <div className=" w-full items-center grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-1 p-5">
-                  {Customers?.map((customer: Customer, index) => (
-                    <div key={index}>
-                      <CustomerCardContainer data={customer} />
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <NotFound text="هنوز مشتری وجود ندارد" height="screen" />
-              )}
-            </>
-          )}
-        </>
+        <div className=" w-full items-center grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-1 p-5">
+          {Customers?.map((customer: Customer, index) => (
+            <div key={index}>
+              <CustomerCardContainer data={customer} />
+            </div>
+          ))}
+        </div>
       ) : (
         <Loading />
       )}
