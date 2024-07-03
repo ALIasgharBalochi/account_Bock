fefd26879bc1bd3e2e578bc31d377ce46f1f0ae3
ali balochi <alibalochi191077@gmail.com>
2024-07-03 19:13:52 +0330

    refactoring body

--- Components/Body.tsx
+++ Components/Body.tsx
@@ -1,52 +1,24 @@
 "use client";
-import { useState, useEffect } from "react";
-import _ from "lodash";
-import CustomerComponent from "./CustomerComponents/CustomerComponent";
-import NavBar from "./Navbar/Navbar";
-import SearchComponent from "./Search/SearchComponent";
-import useStore, { Customer } from "@/store/store";
-import { useFetchCustomers } from "@/dataFetching/fetchCustomersData";
+import { Customer } from "@/store/store";
+import PaginationComponent from "./PaginationComponent";
 type Props = {
   Customers: Customer[];
-  loading: boolean;
+  query: { text: string };
+  searchedCustomers: Customer[];
 };
-const Body: React.FC<Props> = ({ Customers, loading }) => {
-  const [query, setQuery] = useState<{ text: string }>({ text: "" });
-  const [serchedCustomers, setSearchedCustoemr] = useState<Customer[]>([]);
-
-  const { data: customers, isLoading, error } = useFetchCustomers();
-  const setCustomers = useStore((set) => set.setCustomers);
-  useEffect(() => {
-    if (customers) {
-      setCustomers(customers);
-    }
-  }, []);
-
-  const searched: (event: any) => void = (event: any): void => {
-    setQuery({ ...query, text: event.target.value });
-    const allproducts = _.debounce(() => {
-      const filterCustomers = customers.filter((c: Customer) => {
-        return c.name.toLowerCase().includes(event.target.value.toLowerCase());
-      });
-      setSearchedCustoemr(filterCustomers);
-    }, 1000);
-    allproducts();
-  };
-
+const Body: React.FC<Props> = ({ Customers, query, searchedCustomers }) => {
   return (
     <>
-      <NavBar />
       <div className=" w-full h-screen pt-12">
-        <div className=" w-full flex justify-center ">
-          <SearchComponent searched={searched} query={query} />
-        </div>
         <div className=" w-full">
-          <CustomerComponent
-            Customers={Customers}
-            searchedCustomers={serchedCustomers}
-            loading={loading}
-            query={query}
-          />
+          {query.text.length > 0 ? (
+            <PaginationComponent
+              Customers={searchedCustomers}
+              CustomerPerPage={6}
+            />
+          ) : (
+            <PaginationComponent Customers={Customers} CustomerPerPage={6} />
+          )}
         </div>
       </div>
     </>
