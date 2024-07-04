091171e176b240eaf2bd0fae06a74847a590a5c3
ali balochi <alibalochi191077@gmail.com>
2024-07-04 16:16:46 +0330

    add createCustomer func in fetchCustomerdata

--- dataFetching/fetchCustomersData.ts
+++ dataFetching/fetchCustomersData.ts
@@ -17,6 +17,20 @@ export const fetchCustomers = async () => {
 export const useFetchCustomers = () => {
   return useQuery('customers',fetchCustomers)
 }
+// create customer 
+export const createCustomerFunc = async (data:FormData):Promise<Customer> => {
+    const res = await fetch('http://localhost:9000/customers', {
+        method: 'POST',
+        body:data 
+    })
+
+    if (!res.ok) {
+        throw new Error('Network response was not ok')
+    }
+
+    const d = res.json()
+    return d
+}
 
 // gerCustomer by id
 export const fetchCustomerById = async (id:number) => {
