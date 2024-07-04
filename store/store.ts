a4bc8034f8e2668535a7ac077e2326afeb4346cd
ali balochi <alibalochi191077@gmail.com>
2024-07-04 15:59:33 +0330

    reafactoring store

--- store/store.ts
+++ store/store.ts
@@ -1,36 +1,6 @@
 import create from 'zustand';
-// import { persist } from 'zustand/middleware';
 
-export interface Customer {
-  _id: number;
-  name: string;
-  phone: number;
-  photo?:string;
-  totalDebt:number
-  date: string 
-}
-
-export interface Debt {
-    _id: number
-    customer: Customer;
-    itemName:string;
-    number:number;
-    unit:string;
-    part:number;
-    byWhom:string;
-    date: string;
-    discount: string;
-    itemPrice: number
-}
-
-interface StoreState {
-  customers: Customer[];
-  setCustomers: (customers: Customer[]) => void;
-  removeCustomer: (customerId:number) => void
-  getCustomerById: (customerId:number) => Customer | undefined
-  debts: Debt[];
-  setDebt: (debts: Debt[]) => void
-}
+import { Customer,StoreState } from '@/types';
 
 const useStore = create<StoreState>((set,get)=> ({
   customers: [],
