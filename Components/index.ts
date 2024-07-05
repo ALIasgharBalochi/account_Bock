c9ed27ddc1bb265bda98ee635f5a8863229c4222
ali balochi <alibalochi191077@gmail.com>
2024-07-05 17:30:28 +0330

    create index for import and export components

---
+++ Components/index.ts
@@ -0,0 +1,11 @@
+import Layout from "./Layout";
+import PaginationComponent from "./PaginationComponent";
+import PayModal from "./PayModal";
+import ShowTime from "./ShowTime";
+import SpeedDial from "./SpeedDial";
+import Body from "./Body";
+import CustomerContainerPage from "@/app/customer/CustomerComponent/CustomerContainerPage";
+
+
+
+export {Layout,PaginationComponent,PayModal,ShowTime,SpeedDial,Body,CustomerContainerPage}
