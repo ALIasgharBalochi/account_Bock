bca2d89f7498c51cb0430527d254f388872e43da
ali balochi <alibalochi191077@gmail.com>
2024-07-05 19:57:07 +0330

    create modal component

---
+++ Components/Modal.tsx
@@ -0,0 +1,29 @@
+type Props = {
+  openModal: boolean;
+  children: React.ReactNode;
+};
+const Modal: React.FC<Props> = ({ openModal, children }) => {
+  if (!openModal) {
+    return null;
+  }
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        <div className="inline-block align-bottom bg-slate-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
